d505594a83e1e71b8871f44cf123c9ab9b43bd91
zhaop <704340916@qq.com>
2023-07-19 11:01:19 +0800

    优化根据父级容器宽高进行缩放

--- src/components/PrScreenAdapter/PrScreenAdapter.vue
+++ src/components/PrScreenAdapter/PrScreenAdapter.vue
@@ -1,6 +1,6 @@
 <!-- 屏幕适配组件 参数fixedWidth设计稿宽度 fixedHeight设计稿高度-->
 <template>
-  <div class="screen-adapter" :class="[{ 'overflow-hide': overflowHidden }]">
+  <div ref="screenAdapterRef" class="screen-adapter" :class="[{ 'overflow-hide': overflowHidden }]" :style="[{ 'background-color': bg }]">
     <div class="screen-adapter-outer" :style="[StyleScreenAdapterOuter]">
       <div class="screen-adapter-inner" :style="[StyleScreenAdapterInner]">
         <div class="screen-adapter-inner-content">
@@ -12,56 +12,77 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, computed } from 'vue'
+import { ref, computed, onMounted } from 'vue'
+import type { Ref } from 'vue'
 
 const props = defineProps({
   // 期望宽度
   width: {
     type: [Number],
-    default: () => 5760,
+    default: () => 5760
   },
   // 期望高度
   height: {
     type: [Number],
-    default: () => 1620,
+    default: () => 1620
+  },
+  // 自动校准比例
+  autoRatio: {
+    type: [Boolean],
+    default: () => true
   },
   // 最大宽高比 在基数内自动校准宽度 以达到充满两边
   maxAspectRatio: {
     type: [Number],
-    default: () => 4,
+    default: () => 4
   },
   // 超出隐藏 默认开启 关闭时不缩放 但是可以左右滚动
   overflowHidden: {
     type: [Boolean],
-    default: () => true,
+    default: () => true
   },
+  // 背景
+  bg: {
+    type: [String],
+    default: () => 'rgba(5, 21, 39, 0.9)'
+  }
 })
+
+const screenAdapterRef: Ref = ref()
 
 // 要用到的一些参数
 const options = ref({
   width: 0, // 真实宽度
   height: 0, // 真实高度
   innerWidth: 0, // 当前屏幕宽度
-  innerHeight: 0, // 当前屏幕高度
+  innerHeight: 0 // 当前屏幕高度
 })
 
 // 初始化屏幕参数
 const initOptions = () => {
-  const innerWidth = window.innerWidth || document.documentElement.clientWidth
-  const innerHeight = window.innerHeight || document.documentElement.clientHeight
-  let { width, height, maxAspectRatio } = props
+  let dom = screenAdapterRef.value
+  const innerWidth = dom.innerWidth || dom.clientWidth
+  const innerHeight = dom.innerHeight || dom.clientHeight
+  let { width, height, maxAspectRatio, autoRatio } = props
   const ratio = width / height
   let _ratio = innerWidth / innerHeight
   _ratio = Math.min(_ratio, maxAspectRatio)
   // 适配宽度
-  if (ratio < _ratio) {
+  if (autoRatio && ratio < _ratio) {
     width = (width / ratio) * _ratio
   }
   options.value = { ...options.value, width, height, innerWidth, innerHeight }
   // console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:options.value`, options.value)
 }
-initOptions()
-window.addEventListener('resize', initOptions) // 监听外层宽高
+
+// 创建dom监听
+const createObserver = () => {
+  let observer = new ResizeObserver(initOptions)
+  observer.observe(screenAdapterRef.value, { box: 'border-box' })
+}
+onMounted(() => {
+  createObserver()
+})
 
 // 计算缩放
 const Scale = computed(() => {
@@ -87,7 +108,7 @@ const StyleScreenAdapterOuter = computed(() => {
     'padding-top': `${offsetY}px`,
     'padding-bottom': `${offsetY}px`,
     'padding-left': `${offsetX}px`,
-    'padding-right': `${offsetX}px`,
+    'padding-right': `${offsetX}px`
   }
   return style
 })
@@ -98,7 +119,7 @@ const StyleScreenAdapterInner = computed(() => {
   let style = {
     width: `${width}px`,
     height: `${height}px`,
-    transform: `scale(${Scale.value})`,
+    transform: `scale(${Scale.value})`
   }
   return style
 })
@@ -107,10 +128,9 @@ const StyleScreenAdapterInner = computed(() => {
 <style scoped>
 .screen-adapter {
   position: relative;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   overflow: auto;
-  background-color: rgba(5, 21, 39, 0.9);
 }
 .screen-adapter-outer {
   position: relative;
@@ -125,6 +145,7 @@ const StyleScreenAdapterInner = computed(() => {
   position: relative;
   height: 100%;
   transform-origin: center center;
+  overflow: hidden;
 }
 
 .overflow-hide {
